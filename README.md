2cfb5b0ab66412d7c32d014989727b2b8f939bf9
travb0t <122580202+travb0t@users.noreply.github.com>
2023-08-03 21:10:08 -0700

    Adds letter check system and visual element
    Letter check system checks for exact matches only and keeps tally
    of how many exact matches there are.
    
    Boxes to correctly guessed letters added with rough css and html.
    
    Fixed case sensitivity issue when comparing user input to dailyWord.

--- README.md
+++ README.md
@@ -3,12 +3,13 @@ Attempting to making a working facsimile of the popular NYT game World. For the 
 
 Done:
 - create a basic check system that only accepts 5 letter word guesses and indicates a correct guess. 
+- Compare each position of the userGuess and dailyWord strings to determine if: 
+    1. Any letters are correct in the exact position given.
+- Add visual representation of user guesses, each letter having its own box.
 
 To do:
 - Import a 5-letter wordList.
 - Use wordList to randomly determine a dailyWord.
 - Compare userGuess to wordList to determine validity.
 - Compare each position of the userGuess and dailyWord strings to determine if: 
-    1. Any letters are correct in the exact position given.
     2. Any letters match regardless of position.
-- Add visual representation of user guesses, each letter having its own box.
--- twordle.js
+++ twordle.js
@@ -4,19 +4,71 @@
 const button = document.querySelector("button");
 
 function guessWord() {
-    const userGuess = prompt("Please enter a 5 letter word:", "");
-    let dailyWord = "steam";
+    const userGuess = prompt("Please enter a 5 letter word:", "").toLowerCase();
+    let dailyWord = "Steam".toLowerCase();
+
+    const first = document.querySelector(".firstLetter");
+    const second = document.querySelector(".secondLetter");
+    const third = document.querySelector(".thirdLetter");
+    const fourth = document.querySelector(".fourthLetter");
+    const fifth = document.querySelector(".fifthLetter");
+
+    let posOne = "";
+    let posTwo = "";
+    let posThree = "";
+    let posFour = "";
+    let posFive = "";
+    let matches = 0;
 
     if (!/^[a-zA-Z\s]+$/.test(userGuess)) { 
         alert("Please enter a valid word.");
         guessWord()
     } else if (userGuess.length < 5 || userGuess.length > 5) {
-        alert("Your word must contain 5 letters.")
+        alert("Your word must contain 5 letters.");
         guessWord()
     } else if (userGuess == dailyWord) {
-        alert(`${userGuess} is correct!`)
+        alert(`${dailyWord} is correct!`);
+        letterCheck()
     } else {
-        alert("Try again!")
+        alert("Lets check how many letters match...");
+        letterCheck()
+    }
+    function letterCheck() {
+
+        if (userGuess.charAt(0) == dailyWord.charAt(0) && posOne == "") {
+            posOne = userGuess.charAt(0);
+            matches++;
+            first.textContent = posOne;
+            console.log(posOne);
+            letterCheck()
+        } else if (userGuess.charAt(1) == dailyWord.charAt(1) && posTwo == "") {
+            posTwo = userGuess.charAt(1);
+            matches++;
+            second.textContent = posTwo;
+            console.log(posTwo);
+            letterCheck()
+        } else if (userGuess.charAt(2) == dailyWord.charAt(2) && posThree == "") {
+            posThree = userGuess.charAt(2);
+            matches++;
+            third.textContent = posThree;
+            console.log(posThree);
+            letterCheck()
+        } else if (userGuess.charAt(3) == dailyWord.charAt(3) && posFour == "") {
+            posFour = userGuess.charAt(3);
+            matches++;
+            fourth.textContent = posFour;
+            console.log(posFour);
+            letterCheck()
+        } else if (userGuess.charAt(4) == dailyWord.charAt(4) && posFive == "") {
+            posFive = userGuess.charAt(4);
+            matches++;
+            fifth.textContent = posFive;
+            console.log(posFive);
+            letterCheck()
+        } else {
+            alert(`You got ${matches} exact matching letters!`);
+            console.log(matches);
+        }
     }
 }
 

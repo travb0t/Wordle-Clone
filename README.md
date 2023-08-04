4e3925089a6f3b158447372449180f2dbdb329d6
travb0t <122580202+travb0t@users.noreply.github.com>
2023-08-04 12:38:30 -0700

    Adds further letter check functionality/visuals
    Exact matches are now green, and close matches are now orange. Non
    matching letters appear in their field but don't change color.
    
    Compare each position of the userGuess and dailyWord strings to
    determine if:
    Any letters are correct in the exact position given.
    Any letters match regardless of position.
    
    New guesses will overwrite previous guesses.

--- README.md
+++ README.md
@@ -5,11 +5,15 @@ Done:
 - create a basic check system that only accepts 5 letter word guesses and indicates a correct guess. 
 - Compare each position of the userGuess and dailyWord strings to determine if: 
     1. Any letters are correct in the exact position given.
+    2. Any letters match regardless of position.
 - Add visual representation of user guesses, each letter having its own box.
+    1. Exact matches are now green, and close matches are now orange.
+- Non matching letters that have no appearance in the dailyWord will appear correctly in their given position with no color change.
+- Each new entered guess will now correctly overwrite the previous guess.
 
 To do:
 - Import a 5-letter wordList.
 - Use wordList to randomly determine a dailyWord.
 - Compare userGuess to wordList to determine validity.
 - Compare each position of the userGuess and dailyWord strings to determine if: 
-    2. Any letters match regardless of position.
+    3. If there is a letter match not in the exact position, not to give false positives in the case of duplicate letters.
--- twordle.js
+++ twordle.js
@@ -4,6 +4,7 @@
 const button = document.querySelector("button");
 
 function guessWord() {
+
     const userGuess = prompt("Please enter a 5 letter word:", "").toLowerCase();
     let dailyWord = "Steam".toLowerCase();
 
@@ -13,12 +14,28 @@ function guessWord() {
     const fourth = document.querySelector(".fourthLetter");
     const fifth = document.querySelector(".fifthLetter");
 
+    let userOne = userGuess.charAt(0);
+    let userTwo = userGuess.charAt(1);
+    let userThree = userGuess.charAt(2);
+    let userFour = userGuess.charAt(3);
+    let userFive = userGuess.charAt(4);
+
+    let dailyOne = dailyWord.charAt(0);
+    let dailyTwo = dailyWord.charAt(1);
+    let dailyThree = dailyWord.charAt(2);
+    let dailyFour = dailyWord.charAt(3);
+    let dailyFive = dailyWord.charAt(4);
+
     let posOne = "";
     let posTwo = "";
     let posThree = "";
     let posFour = "";
     let posFive = "";
+
     let matches = 0;
+    let closeMatches = 0;
+
+
 
     if (!/^[a-zA-Z\s]+$/.test(userGuess)) { 
         alert("Please enter a valid word.");
@@ -33,46 +50,127 @@ function guessWord() {
         alert("Lets check how many letters match...");
         letterCheck()
     }
+
     function letterCheck() {
 
-        if (userGuess.charAt(0) == dailyWord.charAt(0) && posOne == "") {
-            posOne = userGuess.charAt(0);
+        if (userOne == dailyOne && posOne == "") {
+            posOne = userOne;
             matches++;
             first.textContent = posOne;
             document.getElementById("one").style.backgroundColor = "lime";
             console.log(posOne);
             letterCheck()
-        } else if (userGuess.charAt(1) == dailyWord.charAt(1) && posTwo == "") {
-            posTwo = userGuess.charAt(1);
+        } else if (userTwo == dailyTwo && posTwo == "") {
+            posTwo = userTwo;
             matches++;
             second.textContent = posTwo;
             document.getElementById("two").style.backgroundColor = "lime";
             console.log(posTwo);
             letterCheck()
-        } else if (userGuess.charAt(2) == dailyWord.charAt(2) && posThree == "") {
-            posThree = userGuess.charAt(2);
+        } else if (userThree == dailyThree && posThree == "") {
+            posThree = userThree;
             matches++;
             third.textContent = posThree;
             document.getElementById("three").style.backgroundColor = "lime";
             console.log(posThree);
             letterCheck()
-        } else if (userGuess.charAt(3) == dailyWord.charAt(3) && posFour == "") {
-            posFour = userGuess.charAt(3);
+        } else if (userFour == dailyFour && posFour == "") {
+            posFour = userFour;
             matches++;
             fourth.textContent = posFour;
             document.getElementById("four").style.backgroundColor = "lime";
             console.log(posFour);
             letterCheck()
-        } else if (userGuess.charAt(4) == dailyWord.charAt(4) && posFive == "") {
-            posFive = userGuess.charAt(4);
+        } else if (userFive == dailyFive && posFive == "") {
+            posFive = userFive;
             matches++;
             fifth.textContent = posFive;
             document.getElementById("five").style.backgroundColor = "lime";
             console.log(posFive);
             letterCheck()
         } else {
-            alert(`You got ${matches} exact matching letters!`);
+            closeMatchCheck()
+        }
+    }
+
+    function closeMatchCheck() {
+
+        if (posOne == "" && userOne == dailyTwo || posOne == "" && userOne == dailyThree || posOne == "" && userOne == dailyFour || posOne == "" && userOne == dailyFive) {
+            posOne = userOne;
+            closeMatches++;
+            first.textContent = posOne;
+            document.getElementById("one").style.backgroundColor = "orange";
+            console.log(posOne);
+            closeMatchCheck()
+        } else if (posTwo == "" && userTwo == dailyTwo || posTwo == "" && userTwo == dailyThree || posTwo == "" && userTwo == dailyFour || posTwo == "" && userTwo == dailyFive) {
+            posTwo = userTwo;
+            closeMatches++;
+            second.textContent = posTwo;
+            document.getElementById("two").style.backgroundColor = "orange";
+            console.log(posTwo);
+            closeMatchCheck()
+        } else if (posThree == "" && userThree == dailyTwo || posThree == "" && userThree == dailyThree || posThree == "" && userThree == dailyFour || posThree == "" && userThree == dailyFive) {
+            posThree = userThree;
+            closeMatches++;
+            third.textContent = posThree;
+            document.getElementById("three").style.backgroundColor = "orange";
+            console.log(posThree);
+            closeMatchCheck()
+        } else if (posFour == "" && userFour == dailyTwo || posFour == "" && userFour == dailyThree || posFour == "" && userFour == dailyFour || posFour == "" && userFour == dailyFive) {
+            posFour = userFour;
+            closeMatches++;
+            fourth.textContent = posFour;
+            document.getElementById("four").style.backgroundColor = "orange";
+            console.log(posFour);
+            closeMatchCheck()
+        } else if (posFive == "" && userFive == dailyTwo || posFive == "" && userFive == dailyThree || posFive == "" && userFive == dailyFour || posFive == "" && userFive == dailyFive) {
+            posFive = userFive;
+            closeMatches++;
+            fifth.textContent = posFive;
+            document.getElementById("five").style.backgroundColor = "orange";
+            console.log(posFive);
+            closeMatchCheck()
+        } else {
+            noMatchCheck()
+        }
+    }
+
+    function noMatchCheck() {
+
+        if (posOne == "") {
+            posOne = userOne;
+            first.textContent = posOne;
+            document.getElementById("one").style.backgroundColor = "lightgray";
+            console.log(posOne);
+            noMatchCheck()
+        } else if (posTwo == "") {
+            posTwo = userTwo;
+            second.textContent = posTwo;
+            document.getElementById("two").style.backgroundColor = "lightgray";
+            console.log(posTwo);
+            noMatchCheck()
+        } else if (posThree == "") {
+            posThree = userThree;
+            third.textContent = posThree;
+            document.getElementById("three").style.backgroundColor = "lightgray";
+            console.log(posThree);
+            noMatchCheck()
+        } else if (posFour == "") {
+            posFour = userFour;
+            fourth.textContent = posFour;
+            document.getElementById("four").style.backgroundColor = "lightgray";
+            console.log(posFour);
+            noMatchCheck()
+        } else if (posFive == "") {
+            posFive = userFive;
+            fifth.textContent = posFive;
+            document.getElementById("five").style.backgroundColor = "lightgray";
+            console.log(posFive);
+            noMatchCheck()
+        } else {
+            alert(`You got ${matches} exact matches, and ${closeMatches} close matches!`);
             console.log(matches);
+            console.log(closeMatches);
         }
     }
 }
